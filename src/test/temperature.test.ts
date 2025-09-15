722f796d09f7f2bcb4068b7358e0715005ae5706
Jaden Despeines <jaden.despeines@gmail.com>
2025-09-15 13:55:23 -0400

    temt testing

--- src/test/temperature.test.ts
+++ src/test/temperature.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { isOverheating } from "../temperature";
+import { isOverheating, isUnderheating } from "../temperature";
 
 describe("isOverheating", () => {
   it("returns true if temp is above 70", () => {
@@ -8,5 +8,15 @@ describe("isOverheating", () => {
 
   it("returns false if temp is 70 or below", () => {
     expect(isOverheating(70)).toBe(false);
+  });
+});
+
+describe("isUnderheating", () => {
+  it("returns true if temp is below 0", () => {
+    expect(isUnderheating(-5)).toBe(true);
+  });
+
+  it("returns false if temp is 0 or above", () => {
+    expect(isUnderheating(0)).toBe(false);
   });
 });
