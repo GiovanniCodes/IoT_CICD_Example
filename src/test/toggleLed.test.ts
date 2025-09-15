5872da68806d04f5fd13921303ba17d22fb027da
Floormatt89 <86789069+FloorMatt89@users.noreply.github.com>
2025-09-15 13:52:16 -0400

    Matt Pass test

--- src/test/toggleLed.test.ts
+++ src/test/toggleLed.test.ts
@@ -5,8 +5,4 @@ describe("isOn", () => {
   it("returns true if led is On", () => {
     expect(isOn(true)).toBe(true);
   });
-
-  it("returns false if led is off", () => {
-    expect(isOn(false)).toBe(false);
-  });
 });
